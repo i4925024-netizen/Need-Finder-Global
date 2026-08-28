d4e95868e5c131713bdf5d428fc2c052ebbaf3c7
i4925024-netizen <i4925024@gmail.com>
2026-08-28 17:50:45 +0500

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,8 @@
 const SUPABASE_URL = "https://nfehafoxxjlolvygiwoc.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_0_ODFNTE7dCA_49dosjjCQ_N7mOxnyE";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
